4af182fb468077effc157ba852b025ceabc0119f
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-12-19 23:48:28 +0530

    Update api.js

--- JobListingApp/frontend/src/services/api.js
+++ JobListingApp/frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://joblistingapp-oklm.onrender.com";
 
 export const fetchJobs = async () => {
   const res = await fetch(`${API_URL}/jobs`);
@@ -15,3 +15,4 @@ export const searchJobsByLocation = async (location) => {
   const data = await res.json();
   return data.jobs;
 };
+
